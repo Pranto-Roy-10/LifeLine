bbaa2e2fe93ac5f82ca5ae39bb8b902ab5e6a9e4
Pranto-Roy-10 <prano1112@gmail.com>
2025-12-04 12:03:55 +0600

    necessary modifications

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\DFIT\LifeLine\venv"
+set "VIRTUAL_ENV=C:\Users\ASUS\Desktop\471 proj\LifeLine\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
